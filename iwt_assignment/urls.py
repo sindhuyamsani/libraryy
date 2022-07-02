4b40bdfc96623b6abdb1c467d5dde418af77e06b
Sindhu Yamsani <92909904+sindhuyamsani@users.noreply.github.com>
2022-07-02 13:03:52 +0530

    Update urls.py

--- iwt_assignment/urls.py
+++ iwt_assignment/urls.py
@@ -19,9 +19,15 @@ from library import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('register',views.register,name="register"),
-    path('login',views.login,name="login"),
+    path('signup',views.signup,name="signup"),
+    path('check',views.check,name="check"),
+    path('add',views.add,name="add"),
     path('addbooks',views.addbooks,name="addbooks"),
     path('display',views.display,name="display"),
+    path('status_student',views.status_student,name="status_student"),
+    path('requested_books',views.requested_books,name="requested_books"),
+    path('requesting_book',views.requesting_book,name="requesting_book"),
+    path('update/<int:id>',views.update,name="update"),
+    path('decline/<int:id>',views.decline,name="decline"),
     path('admin/', admin.site.urls),
 ]
